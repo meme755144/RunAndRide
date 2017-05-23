de19e1dee1465edd5a9c71635c29ed3ab879c9f5
ianpan119 <ianpan119@gmail.com>
2017-05-23 14:13:34 +0800

    Add: 連線到Amazon RDS Add: Google OAuth 登入

--- RunAndRide/RunAndRide/App_Start/Startup.Auth.cs
+++ RunAndRide/RunAndRide/App_Start/Startup.Auth.cs
@@ -59,11 +59,11 @@ namespace RunAndRide
             //   appId: "",
             //   appSecret: "");
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "606186558853-2stjublgsu4tfkeekek05u66okko5c3n.apps.googleusercontent.com",
+                ClientSecret = "UPXZJ2Doj6nJbSXLkD5zmJ8f"
+            });
         }
     }
 }
