0f706eb38b144b7c23de0129969c0f6ce3b895ff
潘奕呈 <ianpan119@gmail.com>
2017-06-15 08:30:48 +0800

    add: 回傳會員資料

--- RunAndRide/RunAndRide/Service/WebForm1.aspx.cs
+++ RunAndRide/RunAndRide/Service/WebForm1.aspx.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNet.Identity.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Serialization;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -12,33 +15,39 @@ namespace RunAndRide.Service
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            String userName = Request.QueryString["userName"];
+            String pwd = Request.QueryString["pwd"];
+
+
+            var signinManager = Context.GetOwinContext().GetUserManager<ApplicationSignInManager>();
             var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
-                var signinManager = Context.GetOwinContext().GetUserManager<ApplicationSignInManager>();
+            var result = signinManager.PasswordSignIn(userName, pwd, true, false);
+            
+            switch (result)
+            {
+                case SignInStatus.Success:
+                    var user = manager.FindByName(userName);
+                    //String strResponse = "true\n" + user.Email + "\n" + user.UserName + "\n" + user.PhoneNumber;
+                    //Response.Write("[{\"userName\":\"" + user.UserName + "\",\"email\":\"" + user.Email + "\"}]");
+                    Hashtable hs = new Hashtable();
+                    hs.Add("userID", user.Id);
+                    hs.Add("userName", user.UserName);
+                    hs.Add("userEmail", user.Email);
+                    hs.Add("userPhone", user.PhoneNumber);
+                    
+                    JavaScriptSerializer js = new JavaScriptSerializer();
+                    String response = js.Serialize(hs);
 
-                // 這不算是因為帳戶鎖定而無法登入
-                // 若要讓密碼失效以觸發鎖定，請變更 shouldLockout: true
-                var result = signinManager.PasswordSignIn("test@gmail.com", "P@ssw0rd", true, shouldLockout: false);
+                    Response.Write(response);
+                    break;
+                case SignInStatus.LockedOut:
+                case SignInStatus.RequiresVerification:
+                case SignInStatus.Failure:
+                default:
+                    Response.Write("false"+userName+pwd);
+                    break;
+            }
 
-                switch (result)
-                {
-                    case SignInStatus.Success:
-                        Response.Write("true");
-                        break;
-                    case SignInStatus.LockedOut:
-                        Response.Write("false");
-                        break;
-                    case SignInStatus.RequiresVerification:
-                        //Response.Redirect(String.Format("/Account/TwoFactorAuthenticationSignIn?ReturnUrl={0}&RememberMe={1}", 
-                        //                                Request.QueryString["ReturnUrl"],
-                        //                                true),
-                        //                  true);
-                        break;
-                    case SignInStatus.Failure:
-                    default:
-                        //FailureText.Text = "無效的登入嘗試";
-                        //ErrorMessage.Visible = true;
-                        break;
-                }
         }
     }
 }
