e10763f7b6b78a05f3dd7111d1b739bd8e8227bd
iii <iii@CR1-20>
2017-06-21 15:19:06 +0800

    .

--- RunAndRide/RunAndRide/About2.aspx.cs
+++ RunAndRide/RunAndRide/About2.aspx.cs
@@ -22,26 +22,13 @@ namespace RunAndRide
 
             public void ProcessRequest(HttpContext context)
             {
-                String userId = context.Request["userId"];
-                String startTime = context.Request["startTime"];
-                String SpendTime = context.Request["SpendTime"];
-                String Distance = context.Request["Distance"];
-                String Speed = context.Request["Speed"];
-                String Cal = context.Request["Cal"];
-                String Reward = context.Request["Reward"];
-
+                
+                String userId = ;
                 SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString.ToString());
                 con.Open();
-                string strSQL = "select Latitude, Longitude from TrackHistoryDetail where UserId = @userId and StartTime = @startTime order by @startTime";
+                string strSQL = "select * from TrackHistory where UserId = @userId";
                 SqlCommand cmd = new SqlCommand(strSQL, con);
                 cmd.Parameters.AddWithValue("@userId", userId);
-                cmd.Parameters.AddWithValue("@startTime", startTime);
-                cmd.Parameters.AddWithValue("@SpentTime", SpendTime);
-                cmd.Parameters.AddWithValue("@Distance", Distance);
-                cmd.Parameters.AddWithValue("@Speed", Speed);
-                cmd.Parameters.AddWithValue("@Cal", Cal);
-                cmd.Parameters.AddWithValue("@Reward", Reward);
-
 
                 SqlDataReader reader = cmd.ExecuteReader();
 
@@ -50,8 +37,13 @@ namespace RunAndRide
                 while (reader.Read() && i < 100)
                 {
                     Hashtable ht = new Hashtable();
-                    ht.Add("Latitude", string.Format("{0}", reader["Latitude"]));
-                    ht.Add("Longitude", string.Format("{0}", reader["Longitude"]));
+                    ht.Add("UserId", string.Format("{0}", reader["UserId"]));
+                    ht.Add("StartTime", string.Format("{0:yyyy-MM-dd HH:mm:ss}", reader["StartTime"]));
+                    ht.Add("SpendTime", string.Format("{0}", reader["SpendTime"]));
+                    ht.Add("Distance", string.Format("{0}", reader["Distance"]));
+                    ht.Add("Speed", string.Format("{0}", reader["Speed"]));
+                    ht.Add("Cal", string.Format("{0}", reader["Cal"]));
+                    ht.Add("Reward", string.Format("{0}", reader["Reward"]));
 
                     eventList.Add(ht);
                     i += 1;
@@ -60,9 +52,6 @@ namespace RunAndRide
                 reader.Close();
                 con.Close();
 
-                JavaScriptSerializer ser = new JavaScriptSerializer();
-                String jsonStr = ser.Serialize(eventList);
-                context.Response.Write(jsonStr);
             }
 
             public bool IsReusable
